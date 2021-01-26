bbf9ead27ff7f07e131b83c85376722b1dd864f6
lilongen <lilongen@gmail.com>
2021-01-26 16:53:22 +0800

    add openvpn key crt sh

--- gen_openvpn_client_key_crt.sh
+++ gen_openvpn_client_key_crt.sh
@@ -10,10 +10,21 @@ get_serial() {
     echo -n $new 
 }
 
-name=${1}
-openssl genrsa -out ${name}.key 4096
-openssl req -new -key ${name}.key -out ${name}.csr -subj "/CN=${name}/"
-ser=$(get_serial)
-openssl x509 -req -in ${name}.csr -out ${name}.crt -CA ca.crt -CAkey ca.key -days 3650 -set_serial ${ser}
+outto_name=${1}
+outto="client_config/${outto_name}"
+if [ -d ${outto} ]; then
+    echo "folder ${outto} already exists!!!"
+    exit 1
+fi
 
+mkdir -p ${outto}
+file_name="${outto}/client"
+openssl genrsa -out ${file_name}.key 4096
+openssl req -new -key ${file_name}.key -out ${file_name}.csr -subj "/CN=${outto_name}/"
+ser=$(get_serial)
+openssl x509 -req -in ${file_name}.csr -out ${file_name}.crt -CA ca.crt -CAkey ca.key -days 3650 -set_serial ${ser}
 echo -n ${ser} > serial
+
+cp ca.crt ${outto}/
+cp config.ovpn ${outto}/${outto_name}.ovpn
+cp ta.key ${outto}/
