20778ea7d61c8f4d6e3095de097d2b7aac58ce62
lilongen <lilongen@gmail.com>
2021-03-05 15:15:42 +0800

    add auto modify proto and port feature

--- gen_openvpn_client_key_crt.sh
+++ gen_openvpn_client_key_crt.sh
@@ -10,7 +10,26 @@ get_serial() {
     echo -n $new 
 }
 
+# ;proto tcp
+# proto udp
+modify_proto() {
+    if [ $proto == 'udp' ]; then
+        return
+    fi
+    perl -i -pE 's/^;(proto tcp)/$1/' ${outto_name}.ovpn
+    perl -i -pE 's/^(proto udp)/;$1/' ${outto_name}.ovpn
+}
+
+
+# remote 34.220.63.184 10136
+modify_port() {
+    perl -i -pE 's/^(remote (\d+\.){3}\d+) \d+/$1 '"${port}"'/' ${outto_name}.ovpn
+}
+
+
 outto_name=${1}
+proto=${2}
+port=${3}
 outto="client_config/${outto_name}"
 if [ -d ${outto} ]; then
     echo "folder ${outto} already exists!!!"
@@ -25,6 +44,10 @@ ser=$(get_serial)
 openssl x509 -req -in ${file_name}.csr -out ${file_name}.crt -CA ca.crt -CAkey ca.key -days 3650 -set_serial ${ser}
 echo -n ${ser} > serial
 
+cp config.ovpn ${outto_name}.ovpn
+modify_proto
+modify_port
+
 cp ca.crt ${outto}/
-cp config.ovpn ${outto}/${outto_name}.ovpn
+cp ${outto_name}.ovpn ${outto}/${outto_name}.ovpn
 cp ta.key ${outto}/
