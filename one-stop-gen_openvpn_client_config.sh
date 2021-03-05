44109abd02cda7d8e5178f11b8925b965e99dcbd
lilongen <lilongen@gmail.com>
2021-03-05 15:39:38 +0800

    one-stop wrapper

--- one-stop-gen_openvpn_client_config.sh
+++ one-stop-gen_openvpn_client_config.sh
@@ -2,7 +2,7 @@
 
 outto_name_prefix=${1}
 
-./gen_openvpn_client_key_crt.sh ${outto_name_prefix}_all udp 10181
-./gen_openvpn_client_key_crt.sh ${outto_name_prefix}_all_tcp tcp 10182
-./gen_openvpn_client_key_crt.sh ${outto_name_prefix}_special udp 10183
-./gen_openvpn_client_key_crt.sh ${outto_name_prefix}_special_tcp tcp 10184
+./gen_openvpn_client_key_crt.sh ${outto_name_prefix}-all udp 10181
+./gen_openvpn_client_key_crt.sh ${outto_name_prefix}-all-tcp tcp 10182
+./gen_openvpn_client_key_crt.sh ${outto_name_prefix}-special udp 10183
+./gen_openvpn_client_key_crt.sh ${outto_name_prefix}-special-tcp tcp 10184
